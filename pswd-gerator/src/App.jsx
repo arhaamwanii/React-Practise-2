55c31cc44ce8cddbd028dca57e003d9ceabf8bda
arhaamwanii <arhamwani765@gmail.com>
2023-11-25 01:16:43 +0530

    .

---
+++ pswd-gerator/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState , useCallback } from 'react'
+import './App.css'
+
+function App() {
+ 
+  const [length , setLength] = useState(8);
+  const [numberALlowed , setNumberAllowed] = useState(false);
+  const [charAllowed , setCharAllowed] = useState(false);
+
+  const [password , setPassword] = useState("");
+
+
+  const passwordGenerator = useCallback( () => {
+    let pass = ""
+    let str = "ABCKDEFGHIJKLMNOUPQRSTUVWXYabcdefghijklmnouprstuvqxyz"
+
+    if (numberALlowed) str += "0123456789"
+    if (charAllowed) str += "@-_}{+=#$%&*()"
+
+
+  } , [length, numberALlowed , charAllowed , setPassword] )
+
+
+  return (
+    <>
+    <h1>{length}</h1>
+    </>
+  )
+}
+
+export default App 
+
+
+///arham amin wani
